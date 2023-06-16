9dc4d7e4bec1f02d9d5562c1e6d7361e9b4fe4ba
simbaat-korea <dev@simbaat.com>
2023-06-16 12:55:10 +0900

    Feat: 마무리

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:mask_app/component/category_card.dart';
@@ -22,49 +23,54 @@ class _HomeScreenState extends State<HomeScreen> {
   ScrollController scrollController = ScrollController();
 
   Future<void> fetchData() async {
-    final now = DateTime.now();
-    final fetchTime = DateTime(
-      now.year,
-      now.month,
-      now.day,
-      now.hour,
-    );
-    final box = Hive.box<StatModel>(ItemCode.PM10.name);
-    final recent = box.values.last;
+    try {
+      final now = DateTime.now();
+      final fetchTime = DateTime(
+        now.year,
+        now.month,
+        now.day,
+        now.hour,
+      );
+      final box = Hive.box<StatModel>(ItemCode.PM10.name);
+      final recent = box.values.last;
 
-    if (recent.dataTime.isAtSameMomentAs(fetchTime)) {
-      print('이미 최핀');
-      return;
-    }
-    List<Future> futures = [];
-    for (ItemCode itemCode in ItemCode.values) {
-      //await를 걸지 않는다
-      futures.add(StatRepository.fetchData(
-        itemCode: itemCode,
-      ));
-    }
-    //리스트 안에 future를 모아서 한번에 기다린다, 리스트안에 값을 넣은 순서대로 값을 받을수 있다
-    final results = await Future.wait(futures);
-
-    //결과값 받는법
-    for (var i = 0; i < results.length; i++) {
-      final key = ItemCode.values[i];
-      final value = results[i];
-
-      final box = Hive.box<StatModel>(key.name);
-
-      for (StatModel stat in value) {
-        box.put(stat.dataTime.toString(), stat);
+      if (recent.dataTime.isAtSameMomentAs(fetchTime)) {
+        print('이미 최핀');
+        return;
       }
-
-      final allKeys = box.keys.toList();
-
-      if (allKeys.length > 24) {
-        //start = 시작 인덱스 end = 끝 인덱스
-        final deleteKeys = allKeys.sublist(0, allKeys.length - 24);
-
-        box.deleteAll(deleteKeys);
+      List<Future> futures = [];
+      for (ItemCode itemCode in ItemCode.values) {
+        //await를 걸지 않는다
+        futures.add(StatRepository.fetchData(
+          itemCode: itemCode,
+        ));
       }
+      //리스트 안에 future를 모아서 한번에 기다린다, 리스트안에 값을 넣은 순서대로 값을 받을수 있다
+      final results = await Future.wait(futures);
+
+      //결과값 받는법
+      for (var i = 0; i < results.length; i++) {
+        final key = ItemCode.values[i];
+        final value = results[i];
+
+        final box = Hive.box<StatModel>(key.name);
+
+        for (StatModel stat in value) {
+          box.put(stat.dataTime.toString(), stat);
+        }
+
+        final allKeys = box.keys.toList();
+
+        if (allKeys.length > 24) {
+          //start = 시작 인덱스 end = 끝 인덱스
+          final deleteKeys = allKeys.sublist(0, allKeys.length - 24);
+
+          box.deleteAll(deleteKeys);
+        }
+      }
+    } on DioError catch (e) {
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text('인터넷 연결이 원활하지 않습니다.$e')));
     }
   }
 
@@ -119,41 +125,46 @@ class _HomeScreenState extends State<HomeScreen> {
             ),
             body: Container(
               color: status.primaryColor,
-              child: CustomScrollView(
-                controller: scrollController,
-                slivers: [
-                  MainAppBar(
-                    isExpanded: isExpanded,
-                    region: region,
-                    status: status,
-                    stat: stat,
-                    dateTime: stat.dataTime,
-                  ),
-                  SliverToBoxAdapter(
-                    child: Column(
-                      children: [
-                        CategoryCard(
-                          region: region,
-                          darkColor: status.darkColor,
-                          lightColor: status.lightColor,
-                        ),
-                        const SizedBox(height: 16),
-                        ...ItemCode.values.map((itemCode) {
-                          return Padding(
-                            padding: const EdgeInsets.only(bottom: 8.0),
-                            child: HourlyCard(
-                              itemCode: itemCode,
-                              region: region,
-                              darkColor: status.darkColor,
-                              lightColor: status.lightColor,
-                            ),
-                          );
-                        }).toList(),
-                        const SizedBox(height: 32),
-                      ],
+              child: RefreshIndicator(
+                onRefresh: () async {
+                  await fetchData();
+                },
+                child: CustomScrollView(
+                  controller: scrollController,
+                  slivers: [
+                    MainAppBar(
+                      isExpanded: isExpanded,
+                      region: region,
+                      status: status,
+                      stat: stat,
+                      dateTime: stat.dataTime,
                     ),
-                  )
-                ],
+                    SliverToBoxAdapter(
+                      child: Column(
+                        children: [
+                          CategoryCard(
+                            region: region,
+                            darkColor: status.darkColor,
+                            lightColor: status.lightColor,
+                          ),
+                          const SizedBox(height: 16),
+                          ...ItemCode.values.map((itemCode) {
+                            return Padding(
+                              padding: const EdgeInsets.only(bottom: 8.0),
+                              child: HourlyCard(
+                                itemCode: itemCode,
+                                region: region,
+                                darkColor: status.darkColor,
+                                lightColor: status.lightColor,
+                              ),
+                            );
+                          }).toList(),
+                          const SizedBox(height: 32),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
               ),
             ));
       },
